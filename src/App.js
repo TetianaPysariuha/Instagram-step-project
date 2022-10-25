cbbfc71c48c14e5f4e6a139ef50130bfc6f62076
Максим Галушка <maksgalushka@gmail.com>
2022-10-25 15:36:28 +0300

    feat: add styles for UserName component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import UserName from './components/UserName/UserName';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.body}>
       <UserName
-        image="./url/..."
-        nickname="Elise"
+        image="./images/1.jpg"
+        nickname="test_user123423"
+        additionalString="Пожаловаться"
+        comment="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aliquam asperiores voluptate a molestias ab, reprehenderit illum temporibus ut animi quos cupiditate, esse odio tempore dolorem dignissimos quaerat fugiat expedita enim, distinctio explicabo sed repellendus? Quia sunt accusantium cupiditate ratione."
+      />
+      <UserName
+        image="./images/2.jpg"
+        nickname="test_user1234"
+      />
+      <UserName
+        image="./images/3.jpg"
+        nickname="test_user1"
         additionalString="4 HOURS AGO"
       />
+      <UserName />
     </div>
   );
 }
--- src/components/UserName/UserName.js
+++ src/components/UserName/UserName.js
@@ -2,22 +2,37 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './UserName.module.scss';
 
-function UserName({ image, nickname, additionalString }) {
+function UserName({
+  image, nickname, additionalString, comment,
+}) {
   return (
-    <div>
-      <img src={image} alt="avatar" className={styles.avatar} />
-      <Link to="/" className={nickname}>
-        <span>{nickname}</span>
+    <div className={styles.userNameContainer}>
+      <Link to={`/${nickname}/`} className={styles.avatarLink}>
+        <img src={image} alt="avatar" className={styles.avatar} />
       </Link>
-      <p>{additionalString}</p>
+      <div className={styles.nicknameWrraper}>
+        <div>
+          <Link to={`/${nickname}/`} className={styles.nickname}>
+            <span>{nickname}</span>
+          </Link>
+          <p className={styles.comment}>{comment}</p>
+        </div>
+        {additionalString && <p className={styles.additionalString}>{additionalString}</p>}
+      </div>
     </div>
   );
 }
 
 UserName.prototype = {
-  image: PropTypes.string.isRequired,
+  image: PropTypes.string,
   nickname: PropTypes.string.isRequired,
   additionalString: PropTypes.string,
+  comment: PropTypes.string,
+};
+
+UserName.defaultProps = {
+  image: './images/default-avatar.jpg',
+  additionalString: '',
 };
 
 export default UserName;
