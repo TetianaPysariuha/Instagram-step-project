84be313b0c69287ff023dd051a7524e18663b590
Tetiana Pysariuha <tetyanka.p.tv@gmail.com>
2022-10-28 09:32:29 +0300

    feat: add initial loading data from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 /* eslint-disable no-underscore-dangle */
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Modal from './components/Modal/Modal';
+import { getPosts } from './store/posts/actionCreators';
+import { getUsers } from './store/users/actionCreators';
 
 function App() {
   const { isOpenModal } = useSelector((store) => store.modal);
+  /*   const { posts } = useSelector((store) => store.posts);
+  const { users } = useSelector((store) => store.users); */
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  /*   useEffect(() => {
+    console.log(posts);
+    console.log(users);
+  }, [posts, users]); */
 
   return (
     <div className="App">
